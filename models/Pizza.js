4073987258b80902d6200e172f34dc5717e742e6
Racer <reaganjoseph26@yahoo.com>
2021-03-03 13:59:14 -0500

    pizza model created

---
+++ models/Pizza.js
@@ -0,0 +1,21 @@
+const Pizza = model('Pizza', PizzaSchema)
+
+const PizzaSchema = new Schema({
+    pizzaName: {
+      type: String
+    },
+    createdBy: {
+      type: String
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now
+    },
+    size: {
+      type: String,
+      default: 'Large'
+    },
+    toppings: []
+});
+
+module.exports = Pizza
